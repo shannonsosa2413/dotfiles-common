10bf39366a0c3fd80105eccc1f5c28b681ed4e3b
wray-lee <i@wray7.top>
2026-09-04 04:52:11 +0900

    feat: add common shell aliases
    Co-authored-by: shannonsosa2413 <307604770+shannonsosa2413@users.noreply.github.com>

---
+++ aliases.sh
@@ -0,0 +1,9 @@
+# Common shell aliases
+alias ll='ls -lht'
+alias la='ls -la'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias grep='grep --color=auto'
+alias df='df -h'
+alias free='free -h'
+alias ports='ss -tulanp'
